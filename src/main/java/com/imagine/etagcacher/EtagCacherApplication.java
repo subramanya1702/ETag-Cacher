ce716c6192599053102d90cc0196300d16f0354a
Subramanya k <sk01@onetrust.com>
2022-05-04 23:07:25 +0530

    feat(initial-commit): Initial commit.

---
+++ src/main/java/com/imagine/etagcacher/EtagCacherApplication.java
@@ -0,0 +1,13 @@
+package com.imagine.etagcacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtagCacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtagCacherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imagine/etagcacher/EtagCacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imagine.etagcacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtagCacherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
